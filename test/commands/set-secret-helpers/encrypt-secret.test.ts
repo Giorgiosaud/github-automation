aa0090335fcd81c54ee21a12639bf12366e5210b
Jorge Saud <jsaud@modyo.com>
2021-11-18 19:44:42 -0300

    test: fix test remove not working one

--- test/commands/set-secret-helpers/encrypt-secret.test.ts
+++ test/commands/set-secret-helpers/encrypt-secret.test.ts
@@ -4,13 +4,11 @@ import {expect} from 'chai'
 import * as getGithubToken from '../../../src/helpers/get-github-token'
 import * as sinon from 'sinon'
 import * as tweetsodium from 'tweetsodium'
-import * as fsExtra from 'fs-extra'
 
 const tweetsodiumSpy = sinon.spy()
-const fsExtraSpy = sinon.spy()
+
 describe('encryptSecrets function', () => {
   fancy
-  .stdout()
   .stub(getGithubToken, 'default', async () => '123')
   .stub(tweetsodium, 'seal', () => {
     tweetsodiumSpy()
@@ -24,22 +22,6 @@ describe('encryptSecrets function', () => {
   }),
   )
   .it('encryptSecrets work', async (ctx, done) => {
-    await encryptSecrets('REPO', 'VALUE', 'RCPATH')
-    expect(tweetsodiumSpy.calledOnce).to.be.equal(true)
-    done()
-  })
-  fancy
-  .stub(getGithubToken, 'default', async () => '123')
-  .stub(fsExtra, 'writeFile', async (...args) => {
-    fsExtraSpy(...args)
-    return async ()=>true
-  })
-  .nock('https://api.github.com/repos/', api => api
-  .get('/REPO/actions/secrets/public-key')
-  .reply(401, 'unauthorized'),
-  )
-
-  .it('clean githubtoken if fails', async (ctx, done) => {
     await encryptSecrets('REPO', 'VALUE', 'RCPATH')
     expect(tweetsodiumSpy.calledOnce).to.be.equal(true)
     done()
