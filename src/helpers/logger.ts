0cbc319e722bcd5586513d5ceea1cccbace603ba
Jorge Saud <jsaud@modyo.com>
2021-12-14 12:12:22 -0300

    fix dependency

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,8 +1,8 @@
 const colors = require('colors/safe')
 
-export const preProcessed = (message: string): string => colors.bgBlue.whiteBright(message)
-export const info = (message: string): string => colors.bgBlue.whiteBright(message)
-export const processed = (message: string): string => colors.bgGreen.whiteBright(message)
+export const preProcessed = (message: string): string => colors.bgBlue.white(message)
+export const info = (message: string): string => colors.bgBlue.white(message)
+export const processed = (message: string): string => colors.bgGreen.white(message)
 export const warning = (message: string): string => colors.bgYellow.redBright(message)
-export const normal = (message: string): string => colors.bgBlack.whiteBright(message)
-export const error = (message: string): string => colors.bgRed.yellowBright(message)
+export const normal = (message: string): string => colors.bgBlack.white(message)
+export const error = (message: string): string => colors.bgRed.yellow(message)
