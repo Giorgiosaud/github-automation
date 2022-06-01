2cd6c15346ef76692b2589b736b0f9d784a17676
Jorge Saud <jsaud@modyo.com>
2022-06-01 14:37:36 -0400

    feat: clean
    Signed-off-by: Jorge Saud <jsaud@modyo.com>

--- src/helpers/logger.ts
+++
@@ -1,8 +0,0 @@
-const colors = require('colors/safe')
-
-export const preProcessed = (message: string): string => colors.bgBlue.white(message)
-export const info = (message: string): string => colors.bgBlue.white(message)
-export const processed = (message: string): string => colors.bgGreen.white(message)
-export const warning = (message: string): string => colors.bgYellow.redBright(message)
-export const normal = (message: string): string => colors.bgBlack.white(message)
-export const error = (message: string): string => colors.bgRed.yellow(message)
