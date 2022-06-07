601f0e7e072c1ea1be9b691aa66e6cc900c42f13
Jorge Saud <jsaud@modyo.com>
2022-06-07 13:16:28 -0400

    fix: regex repo name
    Signed-off-by: Jorge Saud <jsaud@modyo.com>

--- src/commands/set-secret/index.ts
+++ src/commands/set-secret/index.ts
@@ -52,7 +52,7 @@ export default class SetSecret extends Command {
       }
 
       const okRepoNames = flags.repositories.every((repo: string) => {
-        return /^.+\/$.+/.test(repo)
+        return /^([\w-])+\/([\w-])+$/.test(repo)
       })
       if (!okRepoNames) {
         throw new Error('The repository string must be of type OWNER/NAME')
