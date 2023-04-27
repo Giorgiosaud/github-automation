1ff0af76b81dcf9e251a25a3f7394105d8ddb63b
Jorge Saud <jsaud@modyo.com>
2023-04-27 15:13:19 -0400

    fix: seal box

--- src/helpers/environments/create-environment.ts
+++ src/helpers/environments/create-environment.ts
@@ -4,10 +4,13 @@ const createEnvironment = async (token:string, organization:string, repository:s
   const octokit = new Octokit({
     auth: token,
   })
-  const response = octokit.request('PUT /repos/{owner}/{repo}/environments/{environment_name}', {
+  const response = octokit.request<string>('PUT /repos/{owner}/{repo}/environments/{environment_name}', {
     owner: organization,
     repo: repository,
     environment_name: environment,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
   })
 
   return response
